21d87ed5bf353ed3be7a3c107107e98b4d345e9e
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-24 20:29:08 +0300

    adding new component in project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Text from './components/Text'
+import Text2 from './components/Text2'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
   <Text />
+  <Text2 name='Amir' age='14' city='Moscow'/>
+  <Text2 name='Salambek' age='17' city='Saint-Petersburg'/>
+  </>
 );
 
 
