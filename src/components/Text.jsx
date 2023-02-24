fe3807991509cb2fc441a9902e2ca7797bc8bc2c
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-24 20:12:09 +0300

    add component Text.jsx

---
+++ src/components/Text.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Text.css';
+
+function Text(){
+    return(
+        <>
+        <h1>Hello world!</h1>
+        <ul className="list">
+            <li className="list-1">Lorem</li>
+            <li className="list-2">ipsun</li>
+            <li className="list-3">expo</li>
+        </ul>        
+        </>
+    )
+}
+
+export default Text
