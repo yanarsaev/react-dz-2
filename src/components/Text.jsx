ec758de5bd3392e11728813f44a2afbcdec24cfd
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-24 20:16:17 +0300

    changed component Text.jsx

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -4,12 +4,12 @@ import './Text.css';
 function Text(){
     return(
         <>
-        <h1>Hello world!</h1>
+        <h1>Hello, world!</h1>
         <ul className="list">
             <li className="list-1">Lorem</li>
             <li className="list-2">ipsun</li>
             <li className="list-3">expo</li>
-        </ul>        
+        </ul>       
         </>
     )
 }
