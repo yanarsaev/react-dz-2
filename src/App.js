4759f4fd938038d5d781aa95dffd8a26908345b2
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-24 20:17:27 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          ReactJS
-        </p>
-      </header>
-    </div>
-  );
+  return 
 }
 
 export default App;
